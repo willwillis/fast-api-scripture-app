92aba03fcc085fb17af44f3dd285135200b57678
will <will.willis@gmail.com>
2025-07-24 22:39:00 -0500

    add eslint config to fix CI

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-unused-vars': 'off',
+  },
+}
